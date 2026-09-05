b96adc2ba1c5d292238019bd2b15da2bafb00f14
Andres Franco Villegas <andres.franco+bac@asesorexternoca.com>
2026-09-04 20:46:10 -0500

    feat: test 6

--- infra/main.tf
+++ infra/main.tf
@@ -4,39 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.4"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.4"
-    }
   }
 }
 
-# 1. Descarga inmutable del artefacto desde JFrog
-data "http" "lambda_zip" {
-  url = "https://trialmww3an.jfrog.io/artifactory/drp-artifacts/lambda/drp-redis-flush/${var.build_number}/drp-redis-flush.zip"
-  
-  request_headers = {
-    Authorization = "Bearer ${var.jfrog_token}"
-  }
-}
-
-# 2. Almacenamiento temporal para que el AWS Provider lo lea
-resource "local_file" "lambda_payload" {
-  content_base64 = data.http.lambda_zip.response_body_base64
-  filename       = "${path.module}/drp-redis-flush.zip"
-}
-
-# 3. Creación de la Lambda
 resource "aws_lambda_function" "redis_flush" {
-  filename         = local_file.lambda_payload.filename
+  filename         = "drp-redis-flush.zip" # Apunta directo al archivo local
   function_name    = "drp-redis-flush"
   role             = var.lambda_execution_role_arn
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = filebase64sha256(local_file.lambda_payload.filename)
+  source_code_hash = filebase64sha256("drp-redis-flush.zip") # Ya existirá físicamente
   runtime          = "python3.9"
   timeout          = 30
   
@@ -48,7 +24,7 @@ resource "aws_lambda_function" "redis_flush" {
   environment {
     variables = {
       REDIS_ENDPOINT = var.redis_endpoint
-      REDIS_PORT     = "${var.redis_port}"
+      REDIS_PORT     = "6379"
       ENVIRONMENT    = var.environment
     }
   }
