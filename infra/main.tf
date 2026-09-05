e8697b0397d64172b1012cb1b0c7d016fbbf2386
Andres Franco Villegas <andres.franco+bac@asesorexternoca.com>
2026-09-04 21:12:59 -0500

    fix: declarar backend s3 en terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,15 @@
 terraform {
+  backend "s3" {} # <-- ESTA LÍNEA ES LA CLAVE FALTANTE
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
