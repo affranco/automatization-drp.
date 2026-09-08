69dc6ee8c149ec6dcc4c25ee311f011109cb2b36
Andres Franco Villegas <andres.franco+bac@asesorexternoca.com>
2026-09-07 21:47:46 -0500

    feat: yo yo yo

--- infra/step_function.tf
+++ infra/step_function.tf
@@ -1,3 +1,6 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
 resource "aws_sfn_state_machine" "drp_orchestrator" {
   name     = "DRP-Redis-Failover-Orchestrator"
   role_arn = aws_iam_role.step_functions_role.arn
@@ -7,26 +10,22 @@ resource "aws_sfn_state_machine" "drp_orchestrator" {
     StartAt = "AprobacionManual"
     States = {
       AprobacionManual = {
-        Type = "Task"
+        Type     = "Task"
         Resource = "arn:aws:states:::lambda:invoke.waitForTaskToken"
         Parameters = {
-          # Este ARN se actualizará cuando creemos la Lambda askUser
-          FunctionName = "arn:aws:lambda:ca-central-1:${data.aws_caller_identity.current.account_id}:function:askUser"
+          FunctionName = "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:askUser"
           Payload = {
-            "TaskToken.$" = "$$.Task.Token"
+            "TaskToken.$"   = "$$.Task.Token"
             "ExecutionId.$" = "$$.Execution.Id"
           }
         }
         Next = "VerificarAprobacion"
       }
       VerificarAprobacion = {
-        Type = "Pass"
+        Type   = "Pass"
         Result = "Aprobado, continuando DRP..."
-        End = false
+        End    = true
       }
     }
   })
 }
-
-# Data source para obtener el ID de la cuenta dinámicamente
-data "aws_caller_identity" "current" {}
